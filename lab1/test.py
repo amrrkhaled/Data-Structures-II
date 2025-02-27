2e6302e8f7726020bacbb7c100bc55786818dccf
Habiba abdel hameed <154561371+habibarezq@users.noreply.github.com>
2025-02-28 00:13:01 +0200

    Added test script for Insertion Sort

--- lab1/test.py
+++ lab1/test.py
@@ -2,7 +2,8 @@ import time
 import random
 import matplotlib.pyplot as plt
 from bubbleSort import bubble_sort
-from selectionSort import selection_sort  
+from selectionSort import selection_sort
+from insertionSort import insertion_sort
 
 def generate_random_array(size):
     return [random.randint(0, 1000) for _ in range(size)]
@@ -18,25 +19,29 @@ def main():
     sizes = [100, 200, 500, 1000, 5000, 10000, 25000]  
     bubble_times = []
     selection_times = []
+    insertion_times=[]
 
     for size in sizes:
         arr = generate_random_array(size)  
         bubble_time = measure_execution_time(bubble_sort, arr)
         selection_time = measure_execution_time(selection_sort, arr)
+        insertion_time=measure_execution_time(insertion_sort,arr)
 
         bubble_times.append(bubble_time)
         selection_times.append(selection_time)
+        insertion_times.append(insertion_time)
 
-        print(f"Array Size: {size}, Bubble Sort Time: {bubble_time:.3f} ms, Selection Sort Time: {selection_time:.3f} ms")
+        print(f"Array Size: {size:<6} Bubble Sort Time: {bubble_time:<10.3f} ms, Selection Sort Time: {selection_time:<10.3f} ms, Insertion Sort Time: {insertion_time:<10.3f} ms")
 
     # Plot results
     plt.figure(figsize=(10, 6))
     plt.plot(sizes, bubble_times, marker='o', linestyle='-', label="Bubble Sort", color='r')
     plt.plot(sizes, selection_times, marker='s', linestyle='-', label="Selection Sort", color='b')
+    plt.plot(sizes, insertion_times, marker='x', linestyle='-', label="Insertion Sort", color='#FFD700')
 
     plt.xlabel("Array Size")
     plt.ylabel("Execution Time (ms)")
-    plt.title("Bubble Sort vs Selection Sort Execution Time (ms)")
+    plt.title("Bubble Sort vs Selection Sort vs Insertion Sort Execution Time (ms)")
     plt.legend()
     plt.grid(True)
     plt.show()
