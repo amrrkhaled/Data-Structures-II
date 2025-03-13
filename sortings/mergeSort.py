2cd1e972d4c2a7dda69702766a3c515362c5a2c0
habibaelghazouly <habibahoussam7@gmail.com>
2025-03-13 06:05:31 +0200

    updated merge_sort with an extra parameter "THRESHOLD"

--- sortings/mergeSort.py
+++ sortings/mergeSort.py
@@ -1,12 +1,18 @@
-def merge_sort(arr):
+import random
+from insertionSort import insertion_sort
+
+def merge_sort(arr , THRESHOLD):
+    if(len(arr) <= THRESHOLD):
         if(len(arr) > 1):
             mid = len(arr) // 2
             leftArray = arr[:mid]
             rightArray = arr[mid:]
 
-            merge_sort(leftArray)
-            merge_sort(rightArray)
+            merge_sort(leftArray , THRESHOLD)
+            merge_sort(rightArray , THRESHOLD)
             merge(leftArray , rightArray , arr)
+    else:
+        insertion_sort(arr)
 
 def merge(leftArray , rightArray , arr):
     leftSize = len(arr) // 2
@@ -35,4 +41,7 @@ def merge(leftArray , rightArray , arr):
         r += 1
         i += 1
 
+def merge_sort_wrapper(arr):
+    THRESHOLD = 0
+    merge_sort(arr , THRESHOLD)
 
