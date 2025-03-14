ea1088557cb2f5926a39353a7437bed66825d92d
habibaelghazouly <habibahoussam7@gmail.com>
2025-03-14 02:33:19 +0200

    updated a condition in hybrid sort

--- sortings/hybridSorting.py
+++ sortings/hybridSorting.py
@@ -3,7 +3,7 @@ from mergeSort import merge
 from insertionSort import insertion_sort            
 
 def merge_sort_hybrid(arr , THRESHOLD):
-    if(len(arr) <= THRESHOLD):
+    if(len(arr) > THRESHOLD):
         if(len(arr) > 1):
             mid = len(arr) // 2
             leftArray = arr[:mid]
@@ -22,7 +22,7 @@ def hybrid_sort(arr , THERSHOLD):
 if __name__ == "__main__":
     SIZE = 50
     THRESHOLD = 6
-    arr = [random.randint(1, 50) for _ in range(SIZE)]
+    arr = [random.randint(1, 100) for _ in range(SIZE)]
     print("Original Array:", arr)
     hybrid_sort(arr, THRESHOLD)
     print("Sorted Array:", arr)
